7a6fc4a4ed150e549538ff289337da4f97797d3a
ExoticPengy <157903768+ExoticPengy@users.noreply.github.com>
2025-03-12 02:30:48 +0800

    Matching stuff

--- src/entity/Match.java
+++ src/entity/Match.java
@@ -1,5 +1,8 @@
 package entity;
 
+import adt.DoublyLinkedList;
+import adt.DoublyLinkedListInterface;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -11,38 +14,95 @@ package entity;
  */
 public class Match {
     private JobPosting jobPosting;
-    private JobSeeker jobSeeker;
-    private int matchScore;
+    private DoublyLinkedListInterface<JobSeeker> jobSeekerList = new DoublyLinkedList<>();
+    private DoublyLinkedListInterface<Double> matchedScoreList = new DoublyLinkedList<>();
 
-    public Match(JobPosting jobPosting, JobSeeker jobSeeker, int matchScore) {
-        this.jobPosting = jobPosting;
-        this.jobSeeker = jobSeeker;
-        this.matchScore = matchScore;
-    }
-
-    public JobPosting getJobPosting() {
-        return jobPosting;
-    }
-
-    public JobSeeker getJobSeeker() {
-        return jobSeeker;
-    }
-
-    public int getMatchScore() {
-        return matchScore;
-    }
-
-    public void setJobPosting(JobPosting jobPosting) {
+    public Match(JobPosting jobPosting) {
         this.jobPosting = jobPosting;
     }
-
-    public void setJobSeeker(JobSeeker jobSeeker) {
-        this.jobSeeker = jobSeeker;
+    
+    public boolean insertMatchScore(double score, int position) {
+        matchedScoreList.insertPosition(score, position);
     }
 
-    public void setMatchScore(int matchScore) {
-        this.matchScore = matchScore;
-    }
-    
-    
+    private boolean calculateMatch() {
+        if (jobSeekerList.isEmpty()) {
+            return false;
+        }
+        
+        int communicationRequirement = 0;
+        int leadershipRequirement = 0;
+        int programmingRequirement = 0;
+        int analysisRequirement = 0;
+        
+        for (int i = 0; i < jobPosting.getSkills().getCount(); i++) {
+            Skill skill = jobPosting.getSkills().getPosition(i);
+            switch(skill.getName()) {
+                case "Communication":
+                    communicationRequirement = skill.getProficiency();
+                    break;
+                case "Leadership":
+                    leadershipRequirement = skill.getProficiency();
+                    break;
+                case "Programming":
+                    programmingRequirement = skill.getProficiency();
+                    break;
+                case "Analysis":
+                    analysisRequirement = skill.getProficiency();
+                    break;
+            }
+        }
+        
+        for (int i = 0; i < jobSeekerList.getCount(); i++) {
+            JobSeeker jobSeeker = jobSeekerList.getPosition(i);
+            double matchScore = 0.0;
+            
+            for (int j = 0; j < jobSeeker.getSkills().getCount(); j++) {
+                Skill skill = jobSeeker.getSkills().getPosition(j);
+                switch(skill.getName()) {
+                    case "Communication":
+                        if (skill.getProficiency() >= communicationRequirement) {
+                            matchScore += (skill.getProficiency() * 2);
+                        }
+                        else {
+                            matchScore += skill.getProficiency();
+                        }
+                        break;
+                    case "Leadership":
+                        if (skill.getProficiency() >= leadershipRequirement) {
+                            matchScore += (skill.getProficiency() * 2);
+                        }
+                        else {
+                            matchScore += skill.getProficiency();
+                        }
+                        break;
+                    case "Programming":
+                        if (skill.getProficiency() >= programmingRequirement) {
+                            matchScore += (skill.getProficiency() * 2);
+                        }
+                        else {
+                            matchScore += skill.getProficiency();
+                        }
+                        break;
+                    case "Analysis":
+                        if (skill.getProficiency() >= analysisRequirement) {
+                            matchScore += (skill.getProficiency() * 2);
+                        }
+                        else {
+                            matchScore += skill.getProficiency();
+                        }
+                        break;
+                    default:
+                        break;
+                } //switch
+            } //inner forloop
+            
+//            if(jobPosting.getLocation().get.equals(jobSeeker.getLocation())) {
+//                matchScore += 10;
+//            }
+            
+            insertMatchScore(matchScore, i + 1);
+        } //outer forloop
+        return true;
+    } //calculateMatch
 }
